92b37f36b803df97b12c9ea814949e8a762df2a6
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-11-16 23:54:55 -0600

    Layout structure for author routes

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -24,4 +24,45 @@ postsRouter.post("/:postid/comments", (req, res, next) => {
     addComment(req, res, next);
 });
 
+
+
+
+// get all posts for author
+postsRouter.get("/author", (req, res, next) => {
+    // confirm we have authorization headers
+    // confirm we have a user with author_role true
+    // return all posts for user.id
+})
+
+// get a post for author
+postsRouter.get("/:postid/author", (req, res, next) => {
+    // confirm we have authorization headers
+    // confirm we have a user with author_role true
+    // return posts.id
+})
+
+// add a post for author
+postsRouter.post("/author", (req, res, next) => {
+    // confirm we have authorization headers
+    // confirm we have a user with author_role true
+    // db.addPost(author_id, title, message, published, timestamp);
+})
+
+// update a post for author
+postsRouter.put("/:postid/author", (req, res, next) => {
+    // confirm we have authorization headers
+    // confirm we have a user with author_role true
+    // db.updatePost(post_id, author_id, title, message, published, timestamp);
+})
+
+// delete post for author
+postsRouter.delete("/:postid/author", (req, res, next) => {
+    // confirm we have authorization headers
+    // confirm we have a user with author_role true
+    // db.deletePost(post_id);
+})
+
+
+
+
 module.exports = postsRouter
